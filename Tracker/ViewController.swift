01947e017953a7c693a4889e5370772b2ed6c448
Denis Kel <asket2013@ya.ru>
2025-06-19 09:16:43 +1000

    Init project

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Денис Кель on 19.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
