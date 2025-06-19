6f61ee5214b59676368ee380fc5b973efb2dba8d
Denis Kel <asket2013@ya.ru>
2025-06-19 17:41:26 +1000

    верстка главной страницы

--- Tracker/MainScreen/TabBarViewController.swift
+++ Tracker/MainScreen/TabBarViewController.swift
@@ -12,7 +12,10 @@ class TabBarViewController: UITabBarController {
         super.viewDidLoad()
         setupViewControllers()
         setupTabBarAppearance()
+        setupConstraints()
     }
+    
+    
     
     private func setupViewControllers() {
         // Первая вкладка - Трекеры
@@ -29,19 +32,26 @@ class TabBarViewController: UITabBarController {
         let statisticsNav = UINavigationController(rootViewController: statisticsVC)
         statisticsNav.tabBarItem = UITabBarItem(
             title: "Статистика",
-            image: UIImage(systemName: "chart.bar.fill"),
-            selectedImage: UIImage(systemName: "chart.bar.fill")
+            image: UIImage(named: "statistic"),
+            selectedImage: UIImage(named: "statistic")
         )
         
         viewControllers = [trackersNav, statisticsNav]
     }
     
     private func setupTabBarAppearance() {
-//        tabBar.tintColor = UIColor(red: 55/255, green: 114/255, blue: 231/255, alpha: 1)
+        tabBar.tintColor = UIColor(red: 55/255, green: 114/255, blue: 231/255, alpha: 1)
         tabBar.unselectedItemTintColor = UIColor(red: 174/255, green: 175/255, blue: 180/255, alpha: 1)
-//        tabBar.backgroundColor = .systemBackground
     }
     
-
+    private func setupConstraints() {
+        
+        NSLayoutConstraint.activate([
+            tabBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 55),
+            tabBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -55),
+            tabBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tabBar.heightAnchor.constraint(equalToConstant: 50),
+        ])
+    }
 }
 
--- Tracker/MainScreen/TrackersViewController.swift
+++ Tracker/MainScreen/TrackersViewController.swift
@@ -9,11 +9,78 @@ import UIKit
 
 
 class TrackersViewController: UIViewController {
+    
+    private let logoImage = UIImageView()
+    private let textClear = UILabel()
+    private let formLabel = UILabel()
+    private let addTrackerButton = UIButton(type: .custom)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let trackerImage = UIImageView(image: UIImage(named: "tracker"))
-        view.backgroundColor = .systemBackground
-        title = "Трекеры"
+        setupClearLogo()
+        setupClearText()
+        setupFormLabel()
+        setupAddTrackerButton()
+        setupConstraint()
+    }
+    
+    private func setupClearLogo() {
+        logoImage.image = UIImage(named: "clearLogo")
+        logoImage.contentMode = .scaleAspectFit
+        logoImage.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoImage)
+    }
+    
+    private func setupClearText(){
+        textClear.text = "Что будем отслеживать?"
+        textClear.textAlignment = .center
+        textClear.font = .systemFont(ofSize: 12, weight: .medium)
+        textClear.textColor = UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 1)
+        textClear.translatesAutoresizingMaskIntoConstraints = false
+        textClear.accessibilityIdentifier = "textClearLabel"
+        view.addSubview(textClear)
+
+    }
+    
+    private func setupFormLabel() {
+        formLabel.text = "Трекеры"
+        formLabel.textAlignment = .left
+        formLabel.font = .systemFont(ofSize: 34, weight: .bold)
+        formLabel.textColor = UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 1)
+        formLabel.translatesAutoresizingMaskIntoConstraints = false
+        formLabel.accessibilityIdentifier = "formLabel"
+        view.addSubview(formLabel)
+    }
+    
+    private func setupAddTrackerButton() {
+        addTrackerButton.setImage(UIImage(named: "add_tracker"), for: .normal)
+        addTrackerButton.translatesAutoresizingMaskIntoConstraints = false
+        addTrackerButton.accessibilityIdentifier = "addTrackerButton"
+        view.addSubview(addTrackerButton)
+
+    }
+    
+    
+    
+    private func setupConstraint (){
+        NSLayoutConstraint.activate([
+            logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImage.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            logoImage.widthAnchor.constraint(equalToConstant: 80),
+            logoImage.heightAnchor.constraint(equalToConstant: 80),
+            textClear.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 8),
+            textClear.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textClear.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            textClear.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            addTrackerButton.widthAnchor.constraint(equalToConstant: 42),
+            addTrackerButton.heightAnchor.constraint(equalToConstant: 42),
+            addTrackerButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 45),
+            addTrackerButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 6),
+            formLabel.topAnchor.constraint(equalTo: addTrackerButton.bottomAnchor, constant: 1),
+            formLabel.leadingAnchor.constraint(equalTo: addTrackerButton.leadingAnchor, constant: 10),
+//            formLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+//            formLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+        ])
+
     }
 }
